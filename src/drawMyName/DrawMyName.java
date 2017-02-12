9b10ef3a9e320703a47a1a4893b4185010dd6ed8
vincentgosselin1 <vincentgosselin1@gmail.com>
2017-02-11 23:44:25 -0500

    first commit

---
+++ src/drawMyName/DrawMyName.java
@@ -0,0 +1,24 @@
+package drawMyName;
+
+public class DrawMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//What is your name?
+		//get 
+		
+		//NameFactory()
+		
+		//NameFactory.make(String name)
+		
+		//Name
+		
+		//Letter
+	
+		//Name.print()
+		
+			
+	}
+
+}
